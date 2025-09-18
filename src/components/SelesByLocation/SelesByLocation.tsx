37b9545111f900d0959b43ccdb6d390327fea4d5
Ahamed Asif <138349258+ahamedasif07@users.noreply.github.com>
2025-09-18 13:26:17 +0600

    map remove

--- src/components/SelesByLocation/SelesByLocation.tsx
+++ src/components/SelesByLocation/SelesByLocation.tsx
@@ -1,18 +1,8 @@
 "use client";
 
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
 
 // Fix default marker icons
-import "leaflet/dist/leaflet.css";
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-});
 
 const locations = [
   { name: "Canada", lat: 56.1304, lng: -106.3468, sales: 75, color: "blue" },
@@ -24,22 +14,6 @@ const SalesByLocationLeaflet = () => {
   return (
     <div className="bg-white w-full  p-4 rounded-2xl  shadow-md">
       <h2 className="text-lg font-semibold mb-4">Sales by Locations</h2>
-
-      <MapContainer
-        center={[20, 0]}
-        zoom={2}
-        scrollWheelZoom={false}
-        style={{ height: "400px", width: "100%" }}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {locations.map((loc) => (
-          <Marker key={loc.name} position={[loc.lat, loc.lng]}>
-            <Popup>
-              {loc.name} - {loc.sales}%
-            </Popup>
-          </Marker>
-        ))}
-      </MapContainer>
 
       {/* Sales Bars below the map */}
       <div className="mt-4 space-y-4">
